bcd383013e534380ebead8aefbd08be74936132b
Akansh Agarwal <Akansh>
2024-01-25 12:32:36 +0530

    landing page

---
+++ components/landing_page/Main_page.jsx
@@ -0,0 +1,94 @@
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
+import React from 'react';
+const pic = require('../images/logo.png');
+const pics = require('../images/toy_faces_1.png');
+const picss = require('../images/toy_faces_2.png');
+const Main_page = () => {
+  return (
+    <View style={{backgroundColor: '#FF4B3A', flex: 1}}>
+      <View
+        style={{
+          width: 73,
+          backgroundColor: 'white',
+          borderRadius: 50,
+          height: 73,
+          marginLeft: 49,
+          marginTop: 56,
+        }}>
+        <Image
+          source={pic}
+          style={{
+            height: 49.65,
+            width: 45.86,
+            marginTop: 12.77,
+            marginLeft: 16.77,
+          }}></Image>
+          
+      </View>
+      <Text style={styles.background}>FOOD FOR EVERYONE</Text>
+      <Image source={pics} style={styles.img} />
+      <Image source={picss} style={styles.imgs} />
+      <View style={styles.button_container}>
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            main_screen;
+          }}>
+          <Text style={styles.text}>GET STARTED</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
+
+export default Main_page
+
+const styles = StyleSheet.create({
+    background: {
+      color: '#FFFFFF',
+      fontSize: 50,
+      marginTop: 30,
+      marginLeft: 51,
+
+    },
+    img: {
+      height: 359.36,
+      width: 200.1,
+      marginTop: 10,
+    },
+    imgs: {
+      height: 298.36,
+      width: 225.1,
+      marginTop: -300,
+      marginLeft: 170,
+    },
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 50,
+      elevation: 3,
+      backgroundColor: '#ffffff',
+      height: 60,
+      width: 271,
+      alignContent: 'center',
+    },
+    text: {
+      fontSize: 20,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: '#FF4B3A',
+    },
+    button_container: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    get_started: {
+      fontSize: 24,
+      color: '#FF4B3A',
+    },
+  });
+  
---
+++ components/landing_page/SliderPage.jsx
@@ -0,0 +1,125 @@
+//import React from 'react';
+import {StyleSheet, View, Image, Text, Pressable} from 'react-native';
+const ellipse = require('../images/ellipse1.png');
+const ellipse2 = require('../images/ellipse2.png');
+const youngpeople = require('../images/young-people-students.png');
+const back = require('../images/back.png')
+const SliderPage = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.main_view}>
+        <View>
+          <View>
+            <Image source={ellipse2} style={styles.circle_2} />
+          </View>
+          <View>
+            <Image source={ellipse} style={styles.circle_1} />
+          </View>
+          <View>
+            <Image source={youngpeople} style={styles.young} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.view_second}>
+        {/* <View style={styles.rectangle}>
+          <Text>Hii</Text>
+        </View> */}
+
+        <View>
+          <Image source={back} style={styles.back_style} />
+          <Text style={styles.welcome}>Welcome to WaraChow</Text>
+          <Text style={styles.describe}>
+            Order the best meals in Lagos and have them delivered to your
+            doorstep in little or no time. Doesn t that sound delicious???
+          </Text>
+          <Pressable style={styles.pressable}>
+            <Text style={styles.next}>Next</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default SliderPage;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#FF4B3A',
+  },
+  main_view: {
+    backgroundColor: '#FF4B3A',
+    flex: 2,
+  },
+  circle_1: {
+    marginLeft: 25,
+    marginTop: 140,
+    width: 233,
+    height: 242,
+    flexShrink: 0,
+    position: 'relative',
+    top: 30,
+  },
+  circle_2: {
+    marginTop: 83,
+    marginLeft: 200,
+    width: 189,
+    height: 191,
+    flexShrink: 0,
+    position: 'absolute',
+  },
+  young: {
+    height: 430,
+    width: 250,
+    bottom: 330,
+    marginLeft: 80,
+  },
+  view_second: {
+    height: 400,
+    width: 400,
+    backgroundColor: '#F9F9F9',
+    alignItems: 'center',
+    flex: 1.3,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  // rectangle:{
+  //   height:110,
+  //   width:4,
+  //   color:'#0F0F0F',
+  //   borderRadius:14,
+
+  // },
+  welcome: {
+    color: '#FA4A0C',
+    textAlign: 'center',
+    fontSize: 24,
+    marginTop: 87,
+  },
+  describe: {
+    color: '#FA4A0C',
+    marginTop: 15,
+    marginLeft:69,
+    marginRight:69,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  pressable:{
+    backgroundColor:'#FF4B3A',
+    width:314,
+    height:70,
+    borderRadius:30,
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:40,
+    marginTop:20,
+  },
+  next:{
+    fontSize:20,
+    color:'#FFF',
+  },
+  back_style:{
+    alignItems:'center',
+  }
+});
